51005e5122751b9c959077b582ce5221622a0875
shea7073 <shea7073@gmail.com>
2021-10-24 10:43:13 -0400

    ready for weekend

--- kivy_udemy/test5.py
+++ kivy_udemy/test5.py
@@ -1,8 +1,45 @@
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.pagelayout import PageLayout
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.stacklayout import StackLayout
+
+
+class Page_Layout(PageLayout):
+    pass
+
+
+class Page_1(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        b1 = Button(text="Hello")
+        b2 = Button(text='World')
+        self.add_widget(b1)
+        self.add_widget(b2)
+
+
+class Page_2(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        b1 = Button(text="Good")
+        b2 = Button(text='Job')
+        self.add_widget(b1)
+        self.add_widget(b2)
+
+
+class Page_3(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        b1 = Button(text="Keep")
+        b2 = Button(text='Going')
+        self.add_widget(b1)
+        self.add_widget(b2)
+
+
+class Relative_Practice(FloatLayout):
+    pass
 
 
 class Interface(FloatLayout):
