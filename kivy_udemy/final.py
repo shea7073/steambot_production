9785d284ed8ecc04601d7b84708427168667a05e
sshea <shea7073@gmail.com>
2021-11-09 16:52:36 -0500

    push from work 11/9

---
+++ kivy_udemy/final.py
@@ -0,0 +1,35 @@
+from kivy.uix.boxlayout import BoxLayout
+from kivymd.app import MDApp
+from kivymd.uix.datatables import MDDataTable
+from kivy.metrics import dp
+
+
+class Main(BoxLayout):
+    pass
+
+
+class Data(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        d = MDDataTable(
+            size_hint=(0.85, 0.85),
+            use_pagination=True,
+
+            column_data=[
+                ('Title', dp(30)),
+                ('Current Price', dp(20)),
+                ('Discount', dp(20)),
+                ('Original Price', dp(22)),
+                ('Release Date', dp(30)),
+                ('Percentage', dp(18)),
+                ('Score', dp(15))
+            ],
+            pos_hint={'center_x': 0.5, 'center_y': 0.5}
+        )
+        self.add_widget(d)
+
+class finish(MDApp):
+    pass
+
+
+finish().run()
--- kivy_udemy/practical1.py
+++ kivy_udemy/practical1.py
@@ -1,21 +1,36 @@
+import random
+
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.button import Button
 
 
+class CanvasTut(BoxLayout):
+    pass
+
 class Game(BoxLayout):
     btn_lst = []
     i = 0
-    def create_btn(self, stacker):
-        btn = Button(text='h', size_hint=(None, None), size=(100, 100))
+    Alpha = ['a', 'b', 'c', 'd', 'e']
+    def create_btn(self):
+        ALPHA = random.choice(self.Alpha)
+        r = random.randrange(1, 10)
+        g = random.randrange(1, 10)
+        b = random.randrange(1, 10)
+        R = r/10
+        G = g/10
+        B = b/10
+        btn = Button(text=str(ALPHA), size_hint=(None, None), size=(100, 100), background_color=(R, G, B, 1), background_normal='')
         self.btn_lst.append(btn)
         self.ids.stacker.add_widget(self.btn_lst[self.i])
         self.i += 1
 
-    def rmv_btn(self, stacker):
-        self.ids.stacker.remove_widget(self.btn_lst[self.i-1])
-        self.i -= 1
+    def rmv_btn(self):
+        if self.i > 0:
+            self.ids.stacker.remove_widget(self.btn_lst[self.i-1])
+            self.btn_lst.pop(self.i-1)
+            self.i -= 1
 
 
 class Buttons(StackLayout):
