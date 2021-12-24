bb3ba1282da672a783320de19655cb3686574604
shea7073 <shea7073@gmail.com>
2021-12-23 20:56:57 -0500

    almost done!

--- final_package/final.py
+++ final_package/final.py
@@ -4,13 +4,39 @@ from kivymd.app import MDApp
 from kivymd.uix.datatables import MDDataTable
 from kivy.metrics import dp
 from kivy.core.window import Window
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.lang import Builder
 import bugs
 import sqlite3
 import os
 import sys
 import scraper
 
+
 Window.size = 1000, 600
+
+
+class FirstWindow(Screen):
+    pass
+
+
+class SecondWindow(Screen):
+    pass
+
+
+class ThirdWindow(Screen):
+    pass
+
+
+class WindowManager(ScreenManager):
+    def go(self, value):
+        self.current = value
+
+    def go2(self, value):
+        self.current = value
+
+    def go3(self, value):
+        self.current = value
 
 
 def load_data(state):
@@ -20,7 +46,7 @@ def load_data(state):
     if state == 1:
         c.execute("SELECT * FROM top_sellers")
     elif state == 2:
-        c.execute("SELECT * FROM top_sellers WHERE word_score = 'Overwhelmingly Positive<'")
+        c.execute("SELECT * FROM top_sellers WHERE word_score = 'Overwhelmingly Positive<' ORDER BY num_score DESC")
     elif state == 3:
         c.execute("SELECT * FROM top_sellers WHERE discount IS NOT NULL")
     else:
@@ -31,16 +57,11 @@ def load_data(state):
     return row_data
 
 
-class Main(BoxLayout):
-    pass
-
-
-class Data(BoxLayout):
+class Data1(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
-        global state
-        row_data = load_data(state)
+        scraper.scrape()
+        row_data = load_data(1)
 
         d = MDDataTable(
             size_hint=(0.85, 0.85),
@@ -61,18 +82,64 @@ class Data(BoxLayout):
         self.add_widget(d)
 
 
-class finish(MDApp):
+class Data2(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        row_data = load_data(2)
+
+        d = MDDataTable(
+            size_hint=(0.85, 0.85),
+            use_pagination=True,
+
+            column_data=[
+                ('Title', dp(60)),
+                ('Current Price', dp(20)),
+                ('Discount', dp(25)),
+                ('Original Price', dp(22)),
+                ('Release Date', dp(30)),
+                ('Percentage', dp(18)),
+                ('Score', dp(15))
+            ],
+            row_data=row_data,
+            pos_hint={'center_x': 0.5, 'center_y': 0.5}
+        )
+        self.add_widget(d)
+
+
+class Data3(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        row_data = load_data(3)
+
+        d = MDDataTable(
+            size_hint=(0.85, 0.85),
+            use_pagination=True,
+
+            column_data=[
+                ('Title', dp(60)),
+                ('Current Price', dp(20)),
+                ('Discount', dp(25)),
+                ('Original Price', dp(22)),
+                ('Release Date', dp(30)),
+                ('Percentage', dp(18)),
+                ('Score', dp(15))
+            ],
+            row_data=row_data,
+            pos_hint={'center_x': 0.5, 'center_y': 0.5}
+        )
+        self.add_widget(d)
+
+
+
+class runner(MDApp):
 
     def build(self):
         self.title = 'SteamBot'
-        return Main()
-
-    def recreate(self):
-        scraper.scrape()
-        self.root.clear_widgets()
-        self.root.add_widget(Main())
-        print('DONE')
+        return Builder.load_file('finish.kv')
 
 
-bugs.fixBugs()
-finish().run()
+if __name__ == '__main__':
+    bugs.fixBugs()
+    runner().run()
